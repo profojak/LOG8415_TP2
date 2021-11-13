ed46aaae7f0bf8cfc7303b7292ca8e4ea41b3195
Ubuntu <profojak@cloud.bq4ag2cnrvkubnn401bxkln4kb.bx.internal.cloudapp.net>
2021-11-13 00:33:57 +0000

    Hadoop vs Spark benchmark

--- wordcount/HadoopSpark.sh
+++ wordcount/HadoopSpark.sh
@@ -29,12 +29,6 @@ OLD_TIMEFORMAT="$TIMEFORMAT"
 TIMEFORMAT='%3U'
 hdfs dfs -mkdir wordcount/output
 
-function average () {
-        ARR=("$@")
-        echo $(IFS='+'; echo "scale=2;(${ARR[*]})/${#ARR[@]}"|bc)
-}
-
-
 for input in input/*.txt; do
         times_spark=()
         times_hadoop=()
@@ -42,25 +36,18 @@ for input in input/*.txt; do
         echo "Benchmarking $FILENAME"
         for i in {1..3}; do
 		echo -n "Spark: "
-                SPARK_TIME=$(
-		time
-		(spark-submit spark.py $input wordcount/output &> /dev/null
-		) )
+		SPARK_TIME=$(time (spark-submit spark.py $input wordcount/output &> /dev/null) )
                 times_spark+=($SPARK_TIME)
 
+		hdfs dfs -rm -r wordcount/output &> /dev/null
+
 		echo -n "Hadoop: "
-                HADOOP_TIME=$(
-		time
-		(hadoop jar /usr/local/hadoop-3.3.1/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar -mapper wordcount/mapper.py -reducer wordcount/reducer.py -input wordcount/input/$input -output wordcount/output &> /dev/null
-		) )
+                HADOOP_TIME=$(time (hadoop jar /usr/local/hadoop-3.3.1/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar -mapper wordcount/mapper.py -reducer wordcount/reducer.py -input wordcount/input/$input -output wordcount/output &> /dev/null) )
                 times_hadoop+=($HADOOP_TIME)
         done
-        AVG=$(average "${times_spark[@]}")
-        echo "Average Spark: $AVG"
-        AVG=$(average "${times_hadoop[@]}")
-        echo "Average Hadoop: $AVG"
 done
 
 TIMEFORMAT="$OLD_TIMEFORMAT"
-hdfs dfs -rm -r wordcount/output
+hdfs dfs -rm -r wordcount
+rm -r input
 /usr/local/hadoop-3.3.1/sbin/stop-all.sh
